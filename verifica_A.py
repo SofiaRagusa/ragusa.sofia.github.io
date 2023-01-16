c622a82bc44542f59e767f61c85c7f2343af6d95
SofiaRagusa <ragusa.sofia@iisgalvanimi.edu.it>
2023-01-16 21:48:19 +0000

    esercizio 5,6 e 7

--- verifica_A.py
+++ verifica_A.py
@@ -1,9 +1,12 @@
+#esercizio 1
 
 import random
 
 z= random.randint(10,20)
 
 print (z)
+
+#esercizio 2
 
 listone1= []
 
@@ -12,14 +15,43 @@ for i in range (z):
 
 print (listone1)
 
+#esercizio 3
+
 listone2=[]
 for x in listone1:
     if x < z:
         listone2.append(x)
 print(listone2)
 
+#esercizio 4
+
 for x in listone1:
     if x in listone2:
         pass
     else:
         print(x)
+
+#esercizio 5 
+listone2quadrato=[]
+for x in listone2:
+    listone2quadrato.append(x*x)
+
+#esercizio  6
+
+risultone=0
+for x in listone2:
+    if x>z:
+        risultone=risultone+x
+
+print(risultone)
+
+#esercizio 7
+
+if risultone+18 > len(listone2):
+    print("evviva")
+else:
+    print("buuu")
+
+
+
+
